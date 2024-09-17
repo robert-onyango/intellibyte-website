b6045e06c272f3cb55b3d2613463d82f457812c1
Robert Onyango <Robert.Onyango@dhcs.ca.gov>
2024-09-17 15:42:07 -0500

    Fixed animations and added contacts and projects

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,13 +1,43 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      title: 'California Department of Health Services',
+      description:
+        'Collaborated on a cloud-native application, reducing release cycles and enhancing code quality.',
+      // image: 'assets/images/DHCSlogo.svg',
+      link: '/projects/dhcs',
+    },
+    {
+      title: 'U.S. Space Force Support',
+      description:
+        'Developed warfighter support applications and migrated IT infrastructure to Azure Kubernetes Service.',
+      // image: 'assets/images/ussf.svg',
+      link: '/projects/ussf',
+    },
+    {
+      title: 'New York City Department of Education',
+      description:
+        'Modernized applications; implemented cloud-native architecture and TDD practices.',
+      // image: 'assets/images/ussf.jpg',
+      link: '/projects/nycdoe',
+    },
+    {
+      title: 'Blackout Startup Modernization',
+      description:
+        'Overhauled backend systems for a disruptive lighting industry startup, enhancing security and efficiency.',
+      // image: 'assets/images/blackout.jpg',
+      link: '/projects/blackout',
+    },
+  ];
 }
--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -1,30 +1,10 @@
 // src/app/route-animations.ts
-
-import { trigger, transition, style, query, animateChild, animate, group } from '@angular/animations';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 export const fader = trigger('routeAnimations', [
   transition('* <=> *', [
-    query(':enter, :leave', [style({ position: 'absolute', width: '100%' })], {
-      optional: true,
-    }),
-    group([
-      query(
-        ':enter',
-        [
-          style({ opacity: 0, transform: 'translateY(10%)' }),
-          animate(
-            '800ms ease',
-            style({ opacity: 1, transform: 'translateY(0)' })
-          ),
-          animateChild(),
-        ],
-        { optional: true }
-      ),
-      query(
-        ':leave',
-        [animate('800ms ease', style({ opacity: 0, transform: 'translateY(-10%)' })), animateChild()],
-        { optional: true }
-      ),
-    ]),
-  ]),
+    style({ opacity: 0 }), // Start with the page invisible
+    animate('500ms ease', style({ opacity: 1 })) // Fade in
+  ])
 ]);
+
